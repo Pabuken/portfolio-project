26fdc9b54d5fa47b043975f872057a8258fec7f9
Pabuken <114421958+Pabuken@users.noreply.github.com>
2022-10-18 17:33:03 -0500

    Change size of hero image based on scroll

--- js/global.js
+++ js/global.js
@@ -1,10 +1,30 @@
 // _____________________ Show/Hide Navbar on Scroll _____________________
 
-window.addEventListener('scroll', (e) => {
-    
+const heroImage = document.getElementById('hero-image')
+let startingWidth = 80;
+let startingHeight = 80;
+
+
+window.addEventListener('wheel', (e) => {
+
+        if (e.deltaY < 0 && startingWidth > 80) {
+            startingWidth -= 3;
+            startingHeight -= 3;
+        } else if (e.deltaY > 0) {
+            startingWidth += 3;
+            startingHeight += 3;
+        };
+
+    heroImage.style.width = `${startingWidth}%`
+    heroImage.style.height = `${startingHeight}%`
+})
+
+window.addEventListener('scroll', () => {
+
     if (window.scrollY >= window.innerHeight/4) {
         document.querySelector('header').style.marginTop = '0'
     } else {
         document.querySelector('header').style.marginTop = ''
     }
+
 })
