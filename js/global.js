cb39e065cd0ead666645789c70b78916c29a9b83
Pabuken <114421958+Pabuken@users.noreply.github.com>
2022-10-19 08:04:27 -0500

    Add dark mode and toggle switch

--- js/global.js
+++ js/global.js
@@ -1,4 +1,9 @@
 
+let switchMode = document.querySelector('input');
 
+switchMode.addEventListener('click', lightMode);
 
-
+function lightMode() {
+    var element = document.body;
+    element.classList.toggle("light-mode");
+  }
